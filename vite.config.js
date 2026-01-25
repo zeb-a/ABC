3787c2834823a00bafe46d325b3984570965aeaa
zeb-a <zebra3003@icloud.com>
2026-01-25 20:33:24 +0800

    urine

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 
   server: {
     host: '0.0.0.0',
-    port: 8080,
+    port: 5173,
     allowedHosts: [ 'classabc.up.railway.app'],
     proxy: {
       '/api/api': {
